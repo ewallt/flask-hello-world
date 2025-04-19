007b9feaf0943de67b6dd67f26e10415b743df07
ewallt <ewallt@yahoo.com>
2025-04-18 22:17:47 -0600

    Update app to run on 0.0.0.0 and dynamic port for Render

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-﻿from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route(\
-/\)
-def hello():
-    return \Hello
-World!\
-
-if __name__ == \__main__\:
-    port = int(os.environ.get(\PORT\, 5000))
-    app.run(host=\0.0.0.0\, port=port)
+﻿from flask import Flask`nimport os`n`napp = Flask(__name__)`n`n@app.route("/")`ndef hello():`n    return "Hello, World!"`n`nif __name__ == "__main__":`n    port = int(os.environ.get("PORT", 5000))`n    app.run(host="0.0.0.0", port=port)
